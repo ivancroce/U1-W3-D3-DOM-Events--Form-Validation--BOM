df60c18aa7bbfadc8622a1fbb823c3c792fb96e7
Ivan Croce <ivan.croce.it@gmail.com>
2025-03-26 17:00:25 +0000

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,37 @@
-window.onload = function () {
-  const form = document.querySelector("form");
-  const tasklist = document.getElementById("tasklist");
+const handleLiClick = function (e) {};
+
+const form = document.querySelector("form");
+const tasklist = document.getElementById("tasklist");
+const inputBox = document.getElementById("input-box");
+
+/* form.addEventListener("submit", function (e) {
+  e.preventDefault(); // very important to remove the refresh
+  console.log(`SUBMIT`);} // to test if refresh works */
+
+form.onsubmit = function (e) {
+  e.preventDefault();
+  console.log(`SUBMIT`);
+
   const inputBox = document.getElementById("input-box");
+  console.log(inputBox.value); // to see in console the task we are applying (text)
 
-  form.addEventListener("submit", function (e) {
-    e.preventDefault();
+  const inputData = inputBox.value;
 
-    const inputBox = document.getElementById("input-box");
+  const newTask = document.createElement("li"); // create a new li in memory
+  newTask.innerText = inputBox.value;
 
-    const inputData = inputBox.value;
+  newTask.onclick = function (e) {
+    e.currentTarget.classList.toggle(`strike`); // toggle the line-through
+  };
 
-    const newTask = document.createElement("li");
-    newTask.textContent = inputData;
+  const delBtn = document.createElement(`button`);
+  delBtn.innerText = "❌"; // need to see this part to add the button
+  delBtn.onclick = handleLiClick;
 
-    tasklist.appendChild(newTask);
-  });
+  newTask.appendChild(delBtn);
+
+  tasklist.appendChild(newTask);
+
+  // form.reset(); // empty all fields
+  inputBox.value = "";
 };
