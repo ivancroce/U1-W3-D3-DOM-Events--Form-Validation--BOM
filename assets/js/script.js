0757acf1fe77a3ca89c86d474ed4b6db8240be07
Ivan Croce <ivan.croce.it@gmail.com>
2025-03-27 00:47:49 +0000

    added some more js and css, need to continue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-const handleLiClick = function (e) {};
-
 const form = document.querySelector("form");
 const tasklist = document.getElementById("tasklist");
 const inputBox = document.getElementById("input-box");
@@ -7,6 +5,12 @@ const inputBox = document.getElementById("input-box");
 /* form.addEventListener("submit", function (e) {
   e.preventDefault(); // very important to remove the refresh
   console.log(`SUBMIT`);} // to test if refresh works */
+
+const handleLiClick = function (e) {
+  e.stopPropagation(); // Prevent any propagation of the same event
+  const taskToRemove = e.target.closest("li");
+  taskToRemove.remove();
+};
 
 form.onsubmit = function (e) {
   e.preventDefault();
@@ -18,14 +22,23 @@ form.onsubmit = function (e) {
   const inputData = inputBox.value;
 
   const newTask = document.createElement("li"); // create a new li in memory
-  newTask.innerText = inputBox.value;
+  newTask.innerText = inputBox.value; // the text will be the input value
 
   newTask.onclick = function (e) {
-    e.currentTarget.classList.toggle(`strike`); // toggle the line-through
+    e.currentTarget.classList.toggle(`strike`); // toggle the line-through on click
   };
 
+  // creating btn and bin img
+
   const delBtn = document.createElement(`button`);
-  delBtn.innerText = "❌"; // need to see this part to add the button
+  const delImg = document.createElement(`img`);
+
+  delImg.setAttribute(`src`, `assets/img/bin.png`);
+  delImg.setAttribute(`alt`, `Delete`);
+  delImg.style.width = "25px";
+  delImg.style.height = "25px";
+
+  delBtn.appendChild(delImg);
   delBtn.onclick = handleLiClick;
 
   newTask.appendChild(delBtn);
@@ -34,4 +47,5 @@ form.onsubmit = function (e) {
 
   // form.reset(); // empty all fields
   inputBox.value = "";
+  inputBox.focus();
 };
